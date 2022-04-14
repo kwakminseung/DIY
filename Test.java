e8b36bde8ed7b224c168efe4cefcbf69cdd4223c
곽민승 <103616574+kwakminseung@users.noreply.github.com>
2022-04-14 14:37:19 +0900

    처음입니다.

--- Test.java
+++ Test.java
@@ -1,5 +1,10 @@
 public class Test {
     public static void main(String[] args) {
         System.out.println("hello world");
+
+        String s="이게 뭐임";
+
+
+        System.out.println("hello world22");
     }
 }
